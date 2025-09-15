ecafed6e1f2385195d5d101f8c21061adc90fe00
Johan <Johanandreasson84@gmail.com>
2025-09-15 22:30:25 +0200

    Add project files.

---
+++ Blazor-Test/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_Test.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_Test.Data
+{
+    public class MyDbContext : DbContext
+{
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; } = default!;
+}
+}
---
+++ Blazor-Test/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_Test.Models
+{
+    public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$")]
+    public string Password { get; set; } = "";
+    
+}
+}
