677a926e5a4ac69fcfa8edc5b3d95becc3de2920
Johan <Johanandreasson84@gmail.com>
2025-09-18 16:01:51 +0200

    Lagt till projekt för test samt ett test

---
+++ Blazor_Test.Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Blazor_Test.Models;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Blazor_Test.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EmailFail()
+        {
+            var user = new User()
+            {
+                FullName = "Okej",
+                UserName = "Okej",
+                Email = "NotOk",
+                Password = "1@rT3456kl!gdsfRT"
+            };
+            var context = new ValidationContext(user);
+                var result = new List<ValidationResult>();
+
+            bool IsOk = Validator.TryValidateObject(user, context, result, true);
+
+            Assert.False(IsOk); //validering inte ok
+            Assert.DoesNotContain(result, r => r.ErrorMessage!.Contains("Email"));
+        }
+        [Fact]
+
+        public void EmailPass()
+        {
+            var user = new User()
+            {
+                FullName = "Okej",
+                UserName = "Okej",
+                Email = "Thisisok@gmail.com",
+                Password = "1@rT3456kl!gdsfRT"
+            };
+            var context = new ValidationContext(user);
+            var result = new List<ValidationResult>();
+
+            bool IsOk = Validator.TryValidateObject(user, context, result, true);
+
+            Assert.True(IsOk); //validering ok
+            Assert.DoesNotContain(result, r => r.ErrorMessage!.Contains("Email"));
+        }
+    }
+}
